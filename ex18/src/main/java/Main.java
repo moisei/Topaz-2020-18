82e8faf955ebaeb867830572ef1b008b5b183d4f
Moisei <moisei@nowhere.com>
2020-12-21 14:03:02 +0200

    num is OK(num) is OK

--- ex18/src/main/java/Main.java
+++ ex18/src/main/java/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static long calc(String expr) {
-        return 1;
+        return calc(expr.toCharArray(), 0, expr.length() - 1);
+    }
+
+    public static long calc(char[] expr, int beg, int end) {
+        char c = expr[beg];
+        if ('(' == c) {
+            int newEnd = end;
+            for (int i = end; i > beg && i != ')'; --i) {
+                newEnd = i;
+            }
+            long innerExpr = calc(expr, beg + 1, newEnd + 1);
+            return innerExpr;
+        } else {
+            int newBeg = beg;
+            for (int i = beg; i <= end && isDigit(expr[i]); ++i) {
+                newBeg = i;
+            }
+            long num = Long.parseLong(new String(expr, beg, newBeg - beg + 1));
+            return num;
+        }
+    }
+
+    private static boolean isDigit(char i) {
+        return i >= '0' && i <= '9';
+    }
+
+    //    interface Token {
+//        TokenType  getType();
+//        TokenValue =
+//    }
+//
+//    private static List<Token> tokenize(String expr) {
+//        Token.
+//        return lst;
+//    }
+//
+    private static String trim(String expr) {
+        return expr;
     }
 }
--- ex18/src/test/java/MainTest.java
+++ ex18/src/test/java/MainTest.java
@@ -4,7 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
     @Test
-    void tst1() {
+    void tstDigit() {
+        assertEquals(0, Main.calc("0"));
         assertEquals(1, Main.calc("1"));
+        assertEquals(2, Main.calc("2"));
+        assertEquals(12, Main.calc("12"));
+        assertEquals(987, Main.calc("987"));
+        for (int i = 0; i < 1000; ++i) {
+            assertEquals(i, Main.calc(String.valueOf(i)));
+        }
+    }
+
+    @Test
+    void tstDigitClose() {
+        assertEquals(0, Main.calc("(0)"));
+        assertEquals(12, Main.calc("(12)"));
     }
 }
